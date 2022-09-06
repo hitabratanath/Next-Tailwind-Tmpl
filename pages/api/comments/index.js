6a81ca4be20f2fb37ba629752032d2e1903efa28
Hita Brata Nath <hitabratanath@gmail.com>
2022-09-07 01:12:02 +0530

    implemented post method

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,4 +1,15 @@
+import { data } from 'autoprefixer'
 import { comments } from '../../../data/comments'
 export default function handler(req, res) {
-    res.status(200).json(comments)
+    if (req.method === 'GET')
+        res.status(200).json(comments)
+    else {
+        const comment = req.body.comment
+        const newComment = {
+            id: Date.now(),
+            comment: comment
+        }
+        comments.push(newComment)
+        res.status(201).json(newComment)
+    }
 }
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,15 +2,30 @@ import { useState } from "react";
 
 const CommentsPage = () => {
     const [comments, setComments] = useState([])
+    const [inputComment, setInputComment] = useState('')
     const loadComment = async () => {
         const response = await fetch('/api/comments')
         const data = await response.json()
         setComments(data)
     }
-
+    const submitComment = async () => {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ comment: inputComment })
+        })
+        const data = response.json()
+        console.log(data);
+    }
     return (
-        <div className="mt-4 w-1/5 mx-auto">
-            <button onClick={loadComment} className='bg-slate-300 p-2 rounded-md font-bold'>Load comments</button>
+        <div className="mt-4 mx-auto">
+            <div className="flex">
+                <input type="text" value={inputComment} onChange={(e) => setInputComment(e.target.value)} />
+                <button onClick={submitComment} className='bg-slate-300 p-2 rounded-md font-bold my-3 ml-2'>Submit comment</button>
+                <button onClick={loadComment} className='bg-slate-300 p-2 rounded-md font-bold my-3 ml-2'>Load comment</button>
+            </div>
             <h2>List of comments:</h2>
             {comments.map(comment => <div key={comment.id}>
                 <p>{comment.comment}</p>
